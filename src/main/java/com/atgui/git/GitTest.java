fad08639cd048267ca7ce10e86a42da12d7191d8
yangpingan <1536858411@qq.com>
2022-04-03 15:03:21 +0800

    first commit

---
+++ src/main/java/com/atgui/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atgui.git;
+
+/**
+ * @ClassName GitTest
+ * @Description: TODO
+ * @Author 15368
+ * @Date 2022/4/3 15:01
+ * @Version:1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
